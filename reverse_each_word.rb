dfa5d2e2a5051ea861487dcafd87731a64d7a55f
john beltrez <beltrezjohn@gmail.com>
2020-07-14 17:49:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   arr = sentence.split
   new_arr = []
-  sentence.each do |word|
-    new_sentence << word.reverse
+  arr.each do |word|
+    new_arr << word.reverse
   end
   new_sentence.join(" ")
 end
