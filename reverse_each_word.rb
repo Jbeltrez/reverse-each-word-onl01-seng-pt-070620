6d8091a02e74bfd436d1805ea6eeb8d0e0ac9385
john beltrez <beltrezjohn@gmail.com>
2020-07-14 17:33:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
 def reverse_each_word(sentence)
-  new
+  new_sentence = []
   sentence.each do |word|
     word.reverse
   end
   
+end
+def hamburger(toppings)
+  my_statements = []
+  toppings.each do |topping|
+    my_statements << "I love #{topping} on my burgers!"
+  end
+  my_statements
 end
